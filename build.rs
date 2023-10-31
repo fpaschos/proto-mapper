a0e58ca20da332f461032dbfc2041f9651dd4f1d
Fotis Paschos <fpaschos@gmail.com>
2023-10-31 19:18:04 +0200

    CI: Fixed protoc test dependencies in case of protobuf

--- build.rs
+++ build.rs
@@ -5,27 +5,29 @@ use std::path::Path;
 fn main() {
     // Generate protobuf crate proto rust code
     let out_dir = std::env::var("OUT_DIR").expect("Unable to get OUT_DIR");
+    #[cfg(feature = "protobuf")]
+    {
+        protobuf_codegen::Codegen::new()
+            .pure()
+            .out_dir(&out_dir)
+            .input("tests/proto/entities.proto")
+            .include("tests/proto")
+            .customize(
+                protobuf_codegen::Customize::default().generate_accessors(true), // .gen_mod_rs(true),
+            )
+            .run_from_script();
 
-    protobuf_codegen::Codegen::new()
-        .pure()
-        .out_dir(&out_dir)
-        .input("tests/proto/entities.proto")
-        .include("tests/proto")
-        .customize(
-            protobuf_codegen::Customize::default().generate_accessors(true), // .gen_mod_rs(true),
-        )
-        .run_from_script();
-
-    // Override the mod file to include all the generated protos
-    let mod_file_content = r#"//@generated
+        // Override the mod file to include all the generated protos
+        let mod_file_content = r#"//@generated
     pub mod entities;
     "#;
-    let mod_file_path = Path::new(&out_dir).join("mod.rs");
+        let mod_file_path = Path::new(&out_dir).join("mod.rs");
 
-    let mut file = fs::File::create(mod_file_path).expect("Unable to create mod.rs file");
-    file.write_all(mod_file_content.to_string().as_ref())
-        .expect("Unable to write mod.rs file");
-
+        let mut file = fs::File::create(mod_file_path).expect("Unable to create mod.rs file");
+        file.write_all(mod_file_content.to_string().as_ref())
+            .expect("Unable to write mod.rs file");
+    }
     // Generate prost crate proto rust code
+    #[cfg(feature = "prost")]
     prost_build::compile_protos(&["tests/proto/entities.proto"], &["/tests"]).unwrap();
 }
--- tests/proto/mod.rs
+++ tests/proto/mod.rs
@@ -1,8 +1,10 @@
+#[cfg(feature="protobuf")]
 pub mod protobuf {
     include!(concat!(env!("OUT_DIR"), "/mod.rs"));
     pub use entities::*;
 }
 
+#[cfg(feature="prost")]
 pub mod prost {
     include!(concat!(env!("OUT_DIR"), "/entities.schema.rs"));
 }
